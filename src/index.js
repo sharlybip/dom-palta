c47105b0ef347f1823443e6c74ab8b694331e89d
Carlos Dominguez <sahrlybip/@gmail.com>
2023-05-08 19:35:47 -0600

    preparando para subir a github

--- src/index.js
+++ src/index.js
@@ -3,4 +3,52 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const baseurl = "https://platzi-avo.vercel.app";
+//web API con promesas
+//conectarnos al servidor
+//procesar la respuesta y convertirla en JSON
+//JSON -> Data -> renderizar
+/////////////////////////////////////////////////////////////////////////
+const appNode =  document.querySelector('#app')
+appNode.classList = 'grid grid-cols-3'
+
+//intl
+//1 - format date
+// format prices
+const formatPrice = price => {
+    const newPrice = new window.Intl.NumberFormat('eS',{
+        style: 'currency',
+        currency: 'EUR'
+    }).format(price);
+
+    return newPrice;
+}
+
+//conectarnos al servidor
+window.fetch(`${baseurl}/api/avo`)
+//procesar la respuesta y convertirla en JSON
+.then(respuesta => respuesta.json())
+//JSON -> Data -> renderizar
+.then(responseJson => {
+    const todosLosItems = [];
+    responseJson.data.map(item => {
+        //crear imagen
+        const image = document.createElement('img');
+        image.className = 'img';
+        image.src = `${baseurl}${item.image}`
+        //crear titulo
+        const title = document.createElement('h2');
+        title.className = 'text-xl text-black-600';
+        title.textContent = item.name;
+        //crear precio
+        const price = document.createElement('div');
+        price.className = 'price';
+        price.textContent = formatPrice(item.price);
+        const container = document.createElement('div');
+        container.className = 'container';
+        container.append(image,title,price);
+        todosLosItems.push(container);
+    })
+    
+    appNode.append(...todosLosItems);
+})
